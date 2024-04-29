f82ae7f01726ffea68178e68ffa9ea8fc4609551
missn0body <hulk128t@gmail.com>
2024-04-29 10:53:58 -0500

    More code refactoring and algorithm changes

--- 1.0.0/src/mcg.asm
+++ 1.0.0/src/mcg.asm
@@ -16,10 +16,36 @@
 ;=================================================
 
 %macro status 1
+	push	rdi
 	mov	rdi, prog_pre
 	call	puts
 	mov	rdi, %1
 	call	puts
+	pop	rdi
+%endmacro
+
+%macro pdec 2
+	push	rdi
+	push	rax
+	mov	rax, [%1]	; retrieve the saved seed
+	mov	rdi, %2		; load in the buffer
+	call	itoa_10		; and convert the number in rax
+	mov	rdi, %2		; reload the clobbered register
+	call	puts		; and print it
+	pop	rax
+	pop	rdi
+%endmacro
+
+%macro phex 2
+	push	rdi
+	push	rax
+	mov	rax, %1		; set number for conversion
+	mov	rdi, %2		; and the buffer
+	call	itoa_16		; convert!
+	mov	rdi, %2		; load the buffer
+	call	puts
+	pop	rax
+	pop	rdi
 %endmacro
 
 ;=================================================
@@ -37,7 +63,6 @@ section .data
 
 	mult		equ 0xBC8F
 	mod		equ 0x7FFFFFFF
-	shiftnum	equ 31
 	regsiz		equ 8
 	bufsiz		equ 32
 
@@ -48,31 +73,37 @@ _start:
 	mov	rax, 0xC9	; we want to get time
 	syscall
 	mov	[seed], rax	; save our seed for later, in case
+	call	mcg		; our argument is already loaded, so call
+	mov	rdi, outbuf	; load in buffer...
+	call	itoa_10		; ... to convert to string
+	mov	rdi, outbuf	; reload buffer...
+	call	puts		; ... to print out
+	mov	rdi, nl		; and also print a newline
+	call	puts
 	call	report		; call a report
 	call	exit_success	; and exit cleanly
 
+; rax = seed
+; mult and mod must already be defined
+mcg:
+	push	rdx		; save any registers we use
+	imul	rax, rax, mult	; A * seed ...
+	mov	rdx, mod	; make mod our divisor
+	div	rax		; and divide
+	mov	rax, rdx	; remainder is in rdx
+	pop	rdx		; and return contents
+	ret
+
 report:
 	status	seed_rep	; see the macro above
-	mov	rax, [seed]	; retrieve the saved seed
-	mov	rdi, seedbuf	; load in the buffer
-	call	itoa_10		; and convert the number in rax
-	mov	rdi, seedbuf	; reload the clobbered register
-	call	puts		; and print it
+	pdec	seed, seedbuf
 	mov	rdi, nl		; with a newline as well
 	call	puts
 	status	mult_rep	; print first part of string
-	mov	rax, mult	; set number for conversion
-	mov	rdi, multbuf	; and the buffer
-	call	itoa_16		; convert!
-	mov	rdi, multbuf	; load the buffer
-	call	puts
+	phex	mult, multbuf
 	mov	rdi, modu_rep	; print first part of string
 	call	puts
-	mov	rax, mod	; set number for conversion
-	mov	rdi, modubuf	; and the buffer
-	call	itoa_16		; convert!
-	mov	rdi, modubuf	; load the buffer
-	call	puts
+	phex	mod, modubuf
 	mov	rdi, nl		; and then put a newline
 	call	puts
 	ret
@@ -82,3 +113,4 @@ section .bss
 	seedbuf	resb	bufsiz
 	multbuf	resb	bufsiz
 	modubuf	resb	bufsiz
+	outbuf	resb	bufsiz
